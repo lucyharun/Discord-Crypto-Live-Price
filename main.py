4ba6083c61dd70553862167c1cdab20aff859a4e
lucyharun <108619102+lucyharun@users.noreply.github.com>
2022-07-11 22:36:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
-# https://github.com/juliankoh/ribbon-discord-bot
-# https://github.com/melenxyz/abracadabra-tvl-bot
+require('dotenv').config() // Load .env file
+const axios = require('axios')
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
-import discord
-from discord.ext import tasks
-import os
-from dotenv import load_dotenv
-import requests
-import json
-import aiohttp
+function getPrices() {
 
-load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-REFRESH_TIMER = os.getenv('REFRESH_TIMER')
-CONTRACT = os.getenv('CONTRACT')
-NAME = os.getenv('NAME')
-CHAIN = os.getenv('CHAIN')
-CURRENCY = os.getenv('CURRENCY')
+	// API for price data.
+	axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${process.env.PREFERRED_CURRENCY}&ids=${process.env.COIN_ID}`).then(res => {
+		// If we got a valid response
+		if(res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
+			let currentPrice = res.data[0].current_price || 0 // Default to zero
+			let priceChange = res.data[0].price_change_percentage_24h || 0 // Default to zero
+			let symbol = res.data[0].symbol || '?' 
+			client.user.setPresence({
+				game: {
+					// Example: "Watching -5,52% | BTC"
+					name: `${priceChange.toFixed(2)}% | ${symbol.toUpperCase()}`,
+					type: 3 // Use activity type 3 which is "Watching"
+				}
+			})
 
-client = discord.Client()
+			client.guilds.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${(currentPrice).toLocaleString().replace(/,/g,process.env.THOUSAND_SEPARATOR)}${process.env.CURRENCY_SYMBOL}`)
 
-async def get_price():
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=creo-engine") as r:
-            if r.status == 200:
-                js = await r.json()
-                price = js[current_price][CURRENCY]
-                pricestring = (f"{NAME}: ${price}")
-                return pricestring
+			console.log('Updated price to', currentPrice)
+		}
+		else
+			console.log('Could not load player count data for', process.env.COIN_ID)
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord! ')
-    for guild in client.guilds:
-        print("connected to ", guild.name)
-    refresh_price.start()
+	}).catch(err => console.log('Error at api.coingecko.com data:', err))
+}
 
-@tasks.loop(seconds=float(REFRESH_TIMER))
-async def refresh_price():
-    for guild in client.guilds:
-        await guild.me.edit(nick=await get_price())
-client.run(TOKEN)
+// Runs when client connects to Discord.
+client.on('ready', () => {
+	console.log('Logged in as', client.user.tag)
+
+	getPrices() // Ping server once on startup
+	// Ping the server and set the new status message every x minutes. (Minimum of 1 minute)
+	setInterval(getPrices, Math.max(1, process.env.MC_PING_FREQUENCY || 1) * 60 * 1000)
+})
+
+// Login to Discord
+client.login(process.env.DISCORD_TOKEN)
