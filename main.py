04a76cdd76245a2322bba76d8e87907e895f748c
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2022-07-13 00:56:10 +0800

    Update main.py
    Signed-off-by: Lucy Harun <108619102+lucyharun@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # https://github.com/juliankoh/ribbon-discord-bot
 # https://github.com/melenxyz/abracadabra-tvl-bot
 
+require('dotenv').config() // Load .env file
+const axios = require('axios')
+const Discord = require('discord.js')
+const client = new Discord.Client()
 import discord
 from discord.ext import tasks
 import os
@@ -19,6 +23,20 @@ CHAIN = os.getenv('CHAIN')
 CURRENCY = os.getenv('CURRENCY')
 
 client = discord.Client()
+
+// API for price data.
+	axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${process.env.PREFERRED_CURRENCY}&ids=${process.env.COIN_ID}`).then(res => {
+		// If we got a valid response
+		if(res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
+			let currentPrice = res.data[0].current_price || 0 // Default to zero
+			let priceChange = res.data[0].price_change_percentage_24h || 0 // Default to zero
+			let symbol = res.data[0].symbol || '?' 
+			client.user.setPresence({
+				game: {
+					// Example: "Watching -5,52% | BTC"
+					name: `${priceChange.toFixed(2)}% | ${symbol.toUpperCase()}`,
+					type: 3 // Use activity type 3 which is "Watching"
+				}
 
 async def get_price():
     async with aiohttp.ClientSession() as session:
